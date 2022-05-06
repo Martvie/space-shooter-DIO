d66a4fd015ce9dfa8d615dc3d6850b3a097ba52d
Martvie <marviedias@gmail.com>
2022-05-06 12:11:24 -0300

    move correction

--- src/js/index.js
+++ src/js/index.js
@@ -2,21 +2,21 @@ const ship = document.querySelector('.hero')
 const playArea = document.querySelector('.game-area')
 
 function moveShip(event) {
-    if (event.key === 'ArrowUp') {
-        event.preventDeafut()
-        moveUp()
-    } else if (event.key === 'ArrowDown') {
-        event.preventDeafut()
-        moveDown()
-    } else if (event.key === 'KeyD') {
-        event.preventDeafut()
-        shoot()
+    if(event.key === 'ArrowUp'|| event.key === 'w') {
+        // event.preventDefault();
+        moveUp();
+    } else if(event.key === 'ArrowDown' || event.key === 's') {
+        // event.preventDefault();
+        moveDown();
+    } else if(event.key === " " || event.key === 'd') {
+        // event.preventDefault();
+        fireLaser();
     }
 }
 
 function moveUp() {
     let topPosition = getComputedStyle(ship).getPropertyValue('top')
-    if (topPosition === '0px') {
+    if(topPosition === "0px") {
         return
     } else {
         let position = parseInt(topPosition)
@@ -27,11 +27,13 @@ function moveUp() {
 
 function moveDown() {
     let topPosition = getComputedStyle(ship).getPropertyValue('top')
-    if (topPosition === '540px') {
+    if(topPosition === "510px"){
         return
     } else {
-        let topPosition = parseInt(position)
+        let position = parseInt(topPosition)
         position += 50
         ship.style.top = `${position}px`
     }
 }
+
+window.addEventListener("keydown", moveShip)
